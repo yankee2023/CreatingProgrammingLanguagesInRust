8cc008fa1a19b799e37b92d2fedaa006a26d7b98
yankee <tad.training2023@gmail.com>
2024-07-28 12:03:13 +0900

    ドキュメンテーションコメントの追加

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -4,5 +4,30 @@ fn main() {
     stack.push(42);
     stack.push(36);
 
+    add(&mut stack);
+
+    stack.push(22);
+
+    add(&mut stack);
+
     println!("stack: {stack:?}");
+}
+
+/// 加算演算
+/// 
+/// # Examples
+/// 
+/// ```
+/// let mut stack = vec![];
+/// stack.push(1);
+/// stack.push(2);
+/// 
+/// assert_eq!(6, add(3));
+///  ```
+fn add(stack: &mut Vec<i32>) {
+    // unwrapは値がOKを前提に取り出すため、
+    // NoneやErrの場合panic!マクロが呼ばれてクラッシュする
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs + rhs);
 }
