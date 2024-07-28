4a6ed881ccb3eee13d25bf9f7b82bc1871978334
yankee <tad.training2023@gmail.com>
2024-07-28 12:28:35 +0900

    2.4パースとコマンドの実行

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -1,10 +1,27 @@
 fn main() {
     // 標準入力の結果を行ごとに取得し、空白区切りで取得
     for line in std::io::stdin().lines() {
+        let mut stack = vec![];
         if let Ok(line) = line {
             // 空白区切り
             let words: Vec<_> = line.split(" ").collect();
-            println!("Line: {words:?}");
+            
+            // 文字列からパースし、数値ならスタックへPush
+            // そうでないなら(四則演算記号)match構文へ
+            for word in words {
+                if let Ok(parsed) = word.parse::<i32>() {
+                    stack.push(parsed);
+                } else {
+                    match word {
+                        "+" => add(&mut stack),
+                        "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
+                        _ => panic!("{word:?} could not be parsed"),
+                    }
+                }
+            }
+            println!("Stack: {stack:?}");
         }
     }
 }
@@ -18,7 +35,7 @@ fn main() {
 /// stack.push(1);
 /// stack.push(2);
 /// 
-/// assert_eq!(6, add(3));
+/// assert_eq!(6, add(&mut stack));
 ///  ```
 fn add(stack: &mut Vec<i32>) {
     // unwrapは値がOKを前提に取り出すため、
@@ -26,4 +43,58 @@ fn add(stack: &mut Vec<i32>) {
     let lhs = stack.pop().unwrap();
     let rhs = stack.pop().unwrap();
     stack.push(lhs + rhs);
+}
+
+/// 減算演算
+/// 
+/// # Examples
+/// 
+/// ```
+/// let mut stack = vec![];
+/// stack.push(3);
+/// stack.push(2);
+/// 
+/// assert_eq!(1, sub(&mut sub));
+/// ```
+fn sub(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs - rhs);
+}
+
+/// 乗算演算
+/// 
+/// # Examples
+/// 
+/// ```
+/// let mut stack = vec![];
+/// stack.push(3);
+/// stack.push(2);
+/// 
+/// assert_eq!(6, sub(&mut mul));
+/// ```
+fn mul(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs * rhs);
+}
+
+/// 除算演算
+/// 
+/// # Examples
+/// 
+/// ```
+/// let mut stack = vec![];
+/// stack.push(6);
+/// stack.push(2);
+/// 
+/// assert_eq!(3, div(&mut sub));
+/// ```
+fn div(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    if rhs == 0 {
+        panic!("Divided by zero.");
+    }
+    stack.push(lhs / rhs);
 }
